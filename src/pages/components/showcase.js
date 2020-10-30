926808357e643750b10cda7af030dce80ac2c41f
AGDholo <agdholo@gmail.com>
2020-10-30 18:05:09 +0800

    feat: redesign theme page

---
+++ src/pages/components/showcase.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import {makeStyles, withStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Tooltip from '@material-ui/core/Tooltip';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import CallMade from '@material-ui/icons/CallMade';
+import Link from '@docusaurus/Link';
+
+import {Row, Column, Item} from '@mui-treasury/components/flex';
+import {useSizedIconButtonStyles} from '@mui-treasury/styles/iconButton/sized';
+
+const StyledTooltip = withStyles({
+  tooltip: {
+    marginTop: '0.2rem',
+    backgroundColor: 'rgba(0,0,0,0.72)',
+    color: '#fff',
+  },
+})(Tooltip);
+
+const useBasicProfileStyles = makeStyles(({palette}) => ({
+  avatar: {
+    borderRadius: 8,
+    backgroundColor: '#fff',
+  },
+  overline: {
+    fontSize: 10,
+    textTransform: 'uppercase',
+    letterSpacing: 1,
+    color: '#8D9CAD',
+  },
+  name: {
+    fontSize: 14,
+    fontWeight: 500,
+    color: '#495869',
+  },
+}));
+
+const BasicProfile = (props) => {
+  const styles = useBasicProfileStyles();
+  return (
+    <Row {...props}>
+      <Item>
+        <Avatar className={styles.avatar}>
+          <img src="feather.svg"></img>
+        </Avatar>
+      </Item>
+      <Item position={'middle'} pl={{sm: 0.5, lg: 0.5}}>
+        <Typography className={styles.overline}>开发者</Typography>
+        <Typography className={styles.name}>HeroUI</Typography>
+      </Item>
+    </Row>
+  );
+};
+
+const useCardHeaderStyles = makeStyles(() => ({
+  root: {paddingBottom: 0},
+  title: {
+    fontSize: '1.25rem',
+    color: '#122740',
+  },
+  subheader: {
+    fontSize: '0.875rem',
+    color: '#495869',
+  },
+}));
+
+const CardHeader = (props) => {
+  const styles = useCardHeaderStyles();
+  const iconBtnStyles = useSizedIconButtonStyles({padding: 8, childSize: 20});
+  return (
+    <Row {...props}>
+      <Item position={'middle'}>
+        <Typography className={styles.title}>
+          <b>{props.text}</b>
+        </Typography>
+        <Typography className={styles.subheader}>{props.desc}</Typography>
+      </Item>
+      <Item position={'right'} mr={-0.5}>
+        <StyledTooltip title={'See details'}>
+          <IconButton classes={iconBtnStyles}>
+            <CallMade />
+          </IconButton>
+        </StyledTooltip>
+      </Item>
+    </Row>
+  );
+};
+
+const useStyles = makeStyles(() => ({
+  card: {
+    border: '2px solid',
+    borderColor: '#E7EDF3',
+    borderRadius: 16,
+    transition: '0.4s',
+    '&:hover': {
+      borderColor: '#5B9FED',
+    },
+  },
+}));
+
+export const ShowcaseCard = React.memo(function ShowcaseCard(props) {
+  const styles = useStyles();
+  const gap = {xs: 1, sm: 1.5, lg: 2};
+  return (
+    <Link to={props.href}>
+      <Column className={styles.card} p={{xs: 0.5, sm: 0.75, lg: 1}} gap={gap}>
+        <CardHeader {...props} />
+        <Item>
+          <Box minHeight={200} bgcolor={'#F4F7FA'} borderRadius={8}>
+            <img src={props.img}></img>
+          </Box>
+        </Item>
+        <BasicProfile />
+      </Column>
+    </Link>
+  );
+});
+export default ShowcaseCard;
--- src/pages/themes.js
+++ src/pages/themes.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import {Button, Grid, Box, Container, SvgIcon} from '@material-ui/core';
+import {Grid, Box, Container, SvgIcon} from '@material-ui/core';
 import Link from '@docusaurus/Link';
-import useBaseUrl from '@docusaurus/useBaseUrl';
 
-import Color from 'color';
-import {makeStyles} from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import {useFourThreeCardMediaStyles} from '@mui-treasury/styles/cardMedia/fourThree';
+import {ShowcaseCard} from './components/showcase';
 
 const feature = [
   '3 个完整应用程序模板',
@@ -40,80 +32,12 @@ const theme = [
   },
 ];
 
-const useGridStyles = makeStyles(({breakpoints}) => ({
-  root: {
-    [breakpoints.up('md')]: {
-      justifyContent: 'center',
-    },
-  },
-}));
-
-const useStyles = makeStyles(() => ({
-  actionArea: {
-    borderRadius: 16,
-    transition: '0.2s',
-    '&:hover': {
-      transform: 'scale(1.1)',
-    },
-  },
-  card: ({color}) => ({
-    minWidth: 256,
-    borderRadius: 16,
-    boxShadow: 'none',
-    '&:hover': {
-      boxShadow: `0 6px 12px 0 ${Color(color)
-        .rotate(-12)
-        .darken(0.2)
-        .fade(0.5)}`,
-    },
-  }),
-  content: ({color}) => {
-    return {
-      backgroundColor: color,
-      padding: '1rem 1.5rem 1.5rem',
-    };
-  },
-  title: {
-    fontFamily: 'Keania One',
-    fontSize: '2rem',
-    color: '#fff',
-    textTransform: 'uppercase',
-  },
-  subtitle: {
-    fontFamily: 'Montserrat',
-    color: '#fff',
-    opacity: 0.87,
-    marginTop: '2rem',
-    fontWeight: 500,
-    fontSize: 14,
-  },
-}));
-
-const CustomCard = ({classes, image, title, subtitle}) => {
-  const mediaStyles = useFourThreeCardMediaStyles();
-  return (
-    <CardActionArea className={classes.actionArea}>
-      <Card className={classes.card}>
-        <CardMedia classes={mediaStyles} image={image} />
-        <CardContent className={classes.content}>
-          <Typography className={classes.title} variant={'h2'}>
-            {title}
-          </Typography>
-        </CardContent>
-      </Card>
-    </CardActionArea>
-  );
-};
-
 export const SolidGameCardDemo = React.memo(function SolidGameCard() {
   const gridStyles = useGridStyles();
   const styles = useStyles({color: '#203f52'});
-  const styles1 = useStyles({color: '#4d137f'});
-  const styles2 = useStyles({color: '#ff9900'});
-  const styles3 = useStyles({color: '#34241e'});
   return (
     <Grid classes={gridStyles} container spacing={4}>
-      {[...theme].map((item, i) => {
+      {[...theme].map((item) => {
         return (
           <Grid item>
             <Link to={item.href}>
@@ -189,7 +113,15 @@ function themes() {
             </Grid>
 
             <Grid item xs={12} md={6}>
-              <SolidGameCardDemo />
+              <Grid container spacing={4}>
+                {[...theme].map((item) => {
+                  return (
+                    <Grid item xs={12} md={6}>
+                      <ShowcaseCard {...item} />
+                    </Grid>
+                  );
+                })}
+              </Grid>
             </Grid>
           </Grid>
         </Box>
